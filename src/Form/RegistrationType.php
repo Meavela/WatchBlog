5f1f5aba18813adacd8342334533fe011ca7c69e
Unknown <wolfla@hotmail.fr>
2019-05-06 14:37:22 +0200

    Modify register

--- src/Form/RegistrationType.php
+++ src/Form/RegistrationType.php
@@ -5,15 +5,30 @@ namespace App\Form;
 
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 class RegistrationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('username')
-                ->add('email')
+        $builder->add('email', EmailType::class, array('label' => 'Email', 'translation_domain' => 'FOSUserBundle'))
+                ->add('username', null, array('label' => 'Username', 'translation_domain' => 'FOSUserBundle'))
+                ->add('plainPassword', RepeatedType::class, array(
+                    'type' => PasswordType::class,
+                    'options' => array(
+                        'translation_domain' => 'FOSUserBundle',
+                        'attr' => array(
+                            'autocomplete' => 'new-password',
+                        ),
+                    ),
+                    'first_options' => array('label' => 'Password'),
+                    'second_options' => array('label' => 'Repeat Password'),
+                    'invalid_message' => 'Mismatch Password',
+                ))
                 ->add('description')
                 ->add('imageFile', FileType::class);
     }
